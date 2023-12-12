e3573102c7627e4ece28f3c7c9ebe83e64534348
laura-zhao <96058202+laura-zhao@users.noreply.github.com>
2023-12-12 16:13:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
+def main(src_dir, dst_dir, geojson_directory, output_directory):
+    # Convert GeoTiff Images to PNG Images
+    src_dir = "/content/drive/MyDrive/AOI_2_Vegas_Train/RGB-PanSharpen/"
+    dst_dir = "/content/drive/MyDrive/AOI_2_Vegas_Train/RGB-PanSharpenNEW/"
+    convert_images(src_dir, dst_dir)
+    
+    #  Extract labels from Geojson files
+    tif_directory = src_dir
+    geojson_directory = '/content/drive/MyDrive/AOI_2_Vegas_Train/geojson/buildings'
+    output_directory = '/content/drive/MyDrive/AOI_2_Vegas_Train/Output Image Directory/'
+    process_geojson(tif_directory, geojson_directory, output_directory, limit=100)
 
-
-def convert_images(src_directory, dst_directory):
-    # Convert Geotiff to PNG Images
-    os.makedirs(dst_directory, exist_ok=True)
-    files = sorted(os.listdir(src_directory))
-
-    for file in files:
-        if file.endswith('.tif'):
-            src_path = os.path.join(src_directory, file)
-            dst_path = os.path.join(dst_directory, file.replace('.tif', '.png'))
-            bit16_to_Bit8(src_path, dst_path)
-
-# Example usage
-src_dir = "/content/drive/MyDrive/AOI_2_Vegas_Train/RGB-PanSharpen/"
-dst_dir = "/content/drive/MyDrive/AOI_2_Vegas_Train/RGB-PanSharpenNEW/"
-convert_images(src_dir, dst_dir)
