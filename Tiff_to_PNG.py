b4d15bffd57322c2fbf9fc481fcc8c6650c235b5
laura-zhao <96058202+laura-zhao@users.noreply.github.com>
2023-12-12 15:39:37 +0800

    Update Tiff_to_PNG.py

--- Tiff_to_PNG.py
+++ Tiff_to_PNG.py
@@ -1,1 +1,41 @@
+from osgeo import gdal
+import numpy as np
+import os
+import subprocess
+
+def bit16_to_Bit8(inputRaster, outputRaster, outputPixType='Byte', outputFormat='png', percentiles=[2, 98]):
+    '''
+    Convert 16bit image to 8bit
+    '''
+  
+    srcRaster = gdal.Open(inputRaster)
+    cmd = ['gdal_translate', '-ot', outputPixType, '-of',
+           outputFormat]
+
+    # iterate through bands
+    for bandId in range(srcRaster.RasterCount):
+        bandId = bandId+1
+        band = srcRaster.GetRasterBand(bandId)
+
+        bmin = band.GetMinimum()
+        bmax = band.GetMaximum()
+        # if not exist minimum and maximum values
+        if bmin is None or bmax is None:
+            (bmin, bmax) = band.ComputeRasterMinMax(1)
+        # else, rescale
+        band_arr_tmp = band.ReadAsArray()
+        bmin = np.percentile(band_arr_tmp.flatten(),
+                             percentiles[0])
+        bmax= np.percentile(band_arr_tmp.flatten(),
+                            percentiles[1])
+
+        cmd.append('-scale_{}'.format(bandId))
+        cmd.append('{}'.format(bmin))
+        cmd.append('{}'.format(bmax))
+        cmd.append('{}'.format(0))
+        cmd.append('{}'.format(255))
+    cmd.append(inputRaster)
+    cmd.append(outputRaster)
+    print("Conversin command:", cmd)
+    subprocess.call(cmd)
 
